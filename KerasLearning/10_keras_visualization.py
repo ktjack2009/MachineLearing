bde5eba183bde9b5d01148617f70a6148e969a34
ktjack2009 <736738213@qq.com>
2018-11-25 23:41:59 +0800

    keras cats_vs_dogs 可视化所有层

--- KerasLearning/10_keras_visualization.py
+++ KerasLearning/10_keras_visualization.py
@@ -19,7 +19,29 @@ model = load_model(model_path)
 layer_outputs = [layer.output for layer in model.layers[:8]]  # 提取前8层的输出
 activation_model = models.Model(inputs=model.input, outputs=layer_outputs)  # 创建一个模型，给定模型输出
 activations = activation_model.predict(img_tensor)
-layer_activation = activations[0]
-for i in range(layer_activation.shape[3]):
-    plt.matshow(layer_activation[0, :, :, i], cmap='viridis')
+
+layer_names = []
+for layer in model.layers[:8]:
+    layer_names.append(layer.name)
+
+images_per_row = 16
+for layer_name, layer_activation in zip(layer_names, activations):
+    n_features = layer_activation.shape[-1]
+    size = layer_activation.shape[1]
+    n_cols = n_features // images_per_row
+    display_grid = np.zeros((size * n_cols, images_per_row * size))
+    for col in range(n_cols):
+        for row in range(images_per_row):
+            channel_image = layer_activation[0, :, :, col * images_per_row + row]
+            channel_image -= channel_image.mean()
+            channel_image /= channel_image.std()
+            channel_image *= 64
+            channel_image += 128
+            channel_image = np.clip(channel_image, 0, 255).astype('uint8')
+            display_grid[col * size:(col + 1) * size, row * size:(row + 1) * size] = channel_image
+    scale = 1. / size
+    plt.figure(figsize=(scale * display_grid.shape[1], scale * display_grid.shape[0]))
+    plt.title(layer_name)
+    plt.grid(False)
+    plt.imshow(display_grid, aspect='auto', cmap='viridis')
 plt.show()
